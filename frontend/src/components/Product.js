1ca71077a540e141c6e7cbf5d821ac002dd1b676
Ridoan Khan Anik <ridoankhan07@gmail.com>
2023-04-05 15:35:14 +0600

    Rating component done

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
@@ -16,7 +17,11 @@ const Product = ({ product }) => {
 
         <Card.Text as='div'>
           <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              // color='red'
+            />
           </div>
         </Card.Text>
         <Card.Text>
